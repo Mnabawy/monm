330ca453be7950ed13f675f7dea9501cce8688f9
Mnabawy <m_ninja61@yahoo.com>
2021-04-19 13:26:19 +0200

    crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/create', function () {
+
+    $user = User::find(1);
+    $role = new Role(['name' => 'user']);
+
+    $user->roles()->save($role);
+});
+
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+
+    foreach ($user->roles as $role) {
+        echo $role->name;
+        echo '</br>';
+    }
+});
+
+
+Route::get('/update' ,function(){
+    $user = User::findOrFail(1);
+    $user->name = 'ahmed';
+    $user->save();
+});
+
+Route::get('/delete', function (){
+    $user = User::findOrFail(1);
+    $user->delete();
 });
